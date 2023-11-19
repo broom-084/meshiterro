6a7657c3b4ed602ef4ae25ceb96e51f5edac5848
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2023-11-19 14:08:39 +0000

    9章

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'homes/about' =>'homes#about', as: 'about'
-  resources :meshiterros
+  
+  resources :post_images, only: [:new, :index, :show]
 
 end
