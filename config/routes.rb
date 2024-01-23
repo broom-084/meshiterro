10ebbfe95f4e061ba71c01bdff01ce01cfcb97fb
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2024-01-23 15:06:26 +0000

    管理者機能3

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,15 @@ Rails.application.routes.draw do
     resources :users, only: [:destroy]
   end
   
-  devise_for :users
-  root to: "homes#top"
-  get 'homes/about' =>'homes#about', as: 'about'
+  scope module: :public do
+    devise_for :users
+    root to: "homes#top"
+    get 'homes/about' =>'homes#about', as: 'about'
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorite, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorite, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
   end
-  resources :users, only: [:show, :edit, :update]
 end
